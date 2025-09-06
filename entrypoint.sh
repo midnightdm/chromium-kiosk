e92333b9949bbface564134e4e6d730e7b7a143d
Mike W <admin@MidnightDigitalMedia.com>
2025-09-06 00:51:07 -0500

    display blanking inhibitor added

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 echo "Launching Chromium in kiosk mode..."
-chromium --no-sandbox \
+systemd-inhibit --what=idle /usr/bin/chromium --no-sandbox \
   --disable-gpu \
   --disable-software-rasterizer \
   --disable-dev-shm-usage \
   --disable-infobars \
   --disable-session-crashed-bubble \
   --disable-features=TranslateUI \
+  --disable-background-timer-throttling \
+  --disable-backgrounding-occluded-windows \
+  --disable-renderer-backgrounding \
+  --disable-gesture-requirement-for-media-playback \
+  --autoplay-policy=no-user-gesture-required \
   --no-first-run \
   --kiosk https://dashboard.riverboattracker.com/dashboard/clinton/100
